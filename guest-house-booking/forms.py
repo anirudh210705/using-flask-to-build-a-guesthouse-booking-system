ed1635305ba6dc30395d17966e19572d9e337aff
Djoker <162271408+anirudh210705@users.noreply.github.com>
2025-03-06 22:09:50 +0530

    Update forms.py

--- guest-house-booking/forms.py
+++ guest-house-booking/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField,SelectField
 from wtforms.validators import Length, DataRequired, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
@@ -11,10 +11,20 @@ class RegistrationForm(FlaskForm):
   #username
   fname = StringField('First Name', validators=[DataRequired(), Length(min = 3, max = 20)])
   lname = StringField('Last Name', validators=[DataRequired(), Length(min = 3, max = 20)])
-  #rollnumber
-  rollnumber = StringField('Roll Number', validators=[DataRequired(), Length(min = 9, max = 9)])
+  username = StringField('User Name', validators=[DataRequired(), Length(max = 20)])
   #email
   email = StringField('Email', validators=[DataRequired(), Email()])
+  #address
+  address = StringField('Address', validators=[DataRequired()])
+  #age 
+  age = StringField('Age', validators=[DataRequired()])
+  gender = SelectField(
+        'Gender', 
+        choices=[('', 'Select Gender'), ('male', 'Male'), ('female', 'Female'), ('other', 'Other')],
+        validators=[DataRequired()]
+    )
+  #rollnumber
+  rollnumber = StringField('Roll Number of Related Student', validators=[DataRequired(), Length(min = 9, max = 9)])
   #password
   password = PasswordField('Password', validators = [DataRequired()])
   confirm_password = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password')])
